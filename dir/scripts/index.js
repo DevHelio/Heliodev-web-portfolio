419829a0f23bb8d102c182f37b8f0f5778dee087
TruFriendsNeverDie <heliodev@outlook.com>
2024-01-04 21:54:29 +0100

    Dropdown menu is working!!!
    (Still have to assign the anchors in each menu item, but we're there.)
    
    Sloppy js code?
    Gives me typerror on line 14 but code works, please God help...

--- dir/scripts/index.js
+++ dir/scripts/index.js
@@ -2,4 +2,16 @@
 
 document
   .getElementById("DropDownButton")
-  .addEventListener("onclick", function () {});
+  .addEventListener("click", function () {
+    console.log("Clicked on dropdown");
+    document.getElementById("DropDownMenuList").style.display = "flex";
+
+    let DropDownMenuList_Items =
+      document.getElementById("DropDownMenuList").children;
+
+    for (kid in DropDownMenuList_Items) {
+      DropDownMenuList_Items[kid].addEventListener("click", () => {
+        document.getElementById("DropDownMenuList").style.display = "none";
+      });
+    }
+  });
